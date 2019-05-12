7f2b73de6fa6c48c11eb90b690a3fec3bd2f7dc2
QiaoRenOreo <46351057+QiaoRenOreo@users.noreply.github.com>
2019-05-12 11:46:11 +0200

    Delete character.d

--- Debug/src/character.d
+++
@@ -1,7 +0,0 @@
-src/character.o: ../src/character.cpp \
- /home/alex/workspace/PACMANfresh/include/character.h \
- /home/alex/workspace/PACMANfresh/include/GameObjectStruct.h
-
-/home/alex/workspace/PACMANfresh/include/character.h:
-
-/home/alex/workspace/PACMANfresh/include/GameObjectStruct.h:
